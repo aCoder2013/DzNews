2c3be33606383a0ff6dd0d83620e5a10b9868d50
BrotherSong <aCoder2013@163.com>
2015-07-25 16:06:59 +0800

    Referesh Content When Scroll to bottom

--- app/src/main/java/com/example/song/dznews/ui/MainActivity.java
+++ app/src/main/java/com/example/song/dznews/ui/MainActivity.java
@@ -3,12 +3,16 @@ package com.example.song.dznews.ui;
 import android.content.Context;
 import android.content.Intent;
 import android.os.PersistableBundle;
+import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.KeyEvent;
@@ -47,6 +51,7 @@ public class MainActivity extends BaseActivity {
     private ActionBarDrawerToggle actionBarDrawerToggle ;
     private NavigationView navigationView;
     private RecyclerView recyclerView;
+    private CoordinatorLayout rootLayout;
     private NewsItemAdapter adapter;
     private List<News> newsList = new ArrayList<>() ;
     @Override
@@ -54,6 +59,7 @@ public class MainActivity extends BaseActivity {
         initTheme();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        rootLayout = (CoordinatorLayout) findViewById(R.id.rootLayout);
         initToolbar();
         initDrawer();
         initNavigation();
@@ -63,11 +69,50 @@ public class MainActivity extends BaseActivity {
     private void initRecyclerView() {
         recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
-        getNewsList(MainActivity.this);
+        getNewsList(MainActivity.this,NewsUtils.CNBETA_NEWS_lIST_URL);
         adapter = new NewsItemAdapter(newsList,MainActivity.this);
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(linearLayoutManager);
         recyclerView.setAdapter(adapter);
+        recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
+
+            }
+
+            @Override
+            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+
+                //得到当前显示的最后一个item的view
+                View lastChildView = recyclerView.getLayoutManager().getChildAt(recyclerView.getLayoutManager().getChildCount()-1);
+                //得到lastChildView的bottom坐标值
+                int lastChildBottom = lastChildView.getBottom();
+                //得到Recyclerview的底部坐标减去底部padding值，也就是显示内容最底部的坐标
+                int recyclerBottom =  recyclerView.getBottom()-recyclerView.getPaddingBottom();
+                //通过这个lastChildView得到这个view当前的position值
+                int lastPosition  = recyclerView.getLayoutManager().getPosition(lastChildView);
+
+                //判断lastChildView的bottom值跟recyclerBottom
+                //判断lastPosition是不是最后一个position
+                //如果两个条件都满足则说明是真正的滑动到了底部
+                if(lastChildBottom == recyclerBottom && lastPosition == recyclerView.getLayoutManager().getItemCount()-1 ){
+                    Snackbar.make(rootLayout,"正在刷新",Snackbar.LENGTH_SHORT).show();
+                    int last_article_id = newsList.get(newsList.size()-1).getArticle_id();
+                    getNewsList(MainActivity.this,NewsUtils.CNBETA_MORE_NEWS_URL+last_article_id);
+                }
+            }
+        });
+    }
+
+    //找到数组中的最大值
+    private int findMax(int[] lastPositions) {
+        int max = lastPositions[0];
+        for (int value : lastPositions) {
+            if (value > max) {
+                max = value;
+            }
+        }
+        return max;
     }
 
     @Override
@@ -176,9 +221,9 @@ public class MainActivity extends BaseActivity {
 
 
 
-    public  void getNewsList(Context context){
+    public  void getNewsList(Context context,String news_url){
 
-        JsonArrayRequest request = new JsonArrayRequest(Request.Method.GET, NewsUtils.CNBETA_NEWS_lIST_URL,
+        JsonArrayRequest request = new JsonArrayRequest(Request.Method.GET, news_url,
                 new Response.Listener<JSONArray>() {
                     @Override
                     public void onResponse(JSONArray response) {
--- app/src/main/java/com/example/song/dznews/utils/NewsUtils.java
+++ app/src/main/java/com/example/song/dznews/utils/NewsUtils.java
@@ -25,7 +25,7 @@ public class NewsUtils {
     //获取首页新闻
     public static final String CNBETA_NEWS_lIST_URL="https://cnbeta1.com/api/getArticles";
     //获取更多新闻
-    public static final String CNBETA_MORE_NEWS_URL="https://cnbeta1.com/api/getMoreArticles/{fromArticleID}";
+    public static final String CNBETA_MORE_NEWS_URL="https://cnbeta1.com/api/getMoreArticles/";
     //获取新闻详情
     public static final String CNBETA_GET_NEWS_DETAIL ="https://cnbeta1.com/api/getArticleDetail/";
 
